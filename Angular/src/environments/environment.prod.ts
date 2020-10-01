5a7fd7391b87a37432dd30981ca31dbc93372b02
KEN\Marc <marc.angers99@gmail.com>
2020-10-01 10:40:04 -0400

    Redeployed to AWS :)

--- Angular/src/environments/environment.prod.ts
+++ Angular/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiURL: 'http://CastleDefenseGame-env-2.acznmbp2nz.us-east-1.elasticbeanstalk.com/'
+  apiURL: 'http://castledefensegame-env.us-east-1.elasticbeanstalk.com/'
 };
